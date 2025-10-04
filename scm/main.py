4663fa6e5b35ea7e139771d700fd520d9236bf3c
Johnny Cai <jaihysc@gmail.com>
2025-10-04 11:10:37 -0400

    Main loop

--- scm/main.py
+++ scm/main.py
@@ -1,71 +1,74 @@
+print('Init -------------------------------------------')
+import cv2
+import numpy as np
 import torch
 import torchvision.transforms as T
 
-from PIL import Image, ImageDraw
+from PIL import Image
 
-import time
+import subprocess
 
 import det
 import camera
-import cv2
-
-
-def draw(images, labels, boxes, scores, thrh = 0.6):
-    for i, im in enumerate(images):
-        draw = ImageDraw.Draw(im)
-
-        scr = scores[i]
-        lab = labels[i][scr > thrh]
-        box = boxes[i][scr > thrh]
-        scrs = scores[i][scr > thrh]
-
-        for j,b in enumerate(box):
-            draw.rectangle(list(b), outline='red',)
-            draw.text((b[0], b[1]), text=f"{lab[j].item()} {round(scrs[j].item(),2)}", fill='green', )
-
-        im.save(f'results_{i}.png')
-
 
 def main():
-    '''
-    im_file = R'./test.png'
-    device = 'cpu' # cpu, cuda:0
+    CAMERA_IMAGE = 'camera_capture.png'
+    DEBUG_IMAGE = 'debug_capture.png'
+    DEVICE = 'cpu' # cpu, cuda:0
+    INF_TH = 0.5 # Inference threshold
 
-    print('DET Model Init ----------------------------------------')
+    print('Load DET Model')
+    model = det.Model().to(DEVICE)
 
-    model = det.Model().to(device)
-
-    print('DET Model Init Done')
-
-
-    print('Runtime ----------------------------------------')
-
-    im_pil = Image.open(im_file).convert('RGB')
-    w, h = im_pil.size
-    orig_size = torch.tensor([w, h])[None].to(device)
-
-    transforms = T.Compose([
-        T.Resize((640, 640)),
-        T.ToTensor(),
-    ])
-    im_data = transforms(im_pil)[None].to(device)
-
-    tStart = time.time()
-    output = model(im_data, orig_size)
-    tEnd = time.time()
-    print(f'Inference time {tEnd - tStart}')
-
-    labels, boxes, scores = output
-
-    draw([im_pil], labels, boxes, scores)
-    '''
+    print('Load localization')
     loc = camera.Localize()
 
-    debugImg = cv2.imread('test3.png')
-    loc.localize(debugImg)
+    print('Runtime ----------------------------------------')
+    iteration = 0
+    while True:
+        # Capture image from camera
+        subprocess.run([
+            'rpicam-still',
+            '--width', '1920',
+            '--height', '1080',
+            '--encoding', 'png',
+            '--output', CAMERA_IMAGE], stdout=subprocess.DEVNULL)
+        print(f'[{iteration}] Captured image')
 
-    cv2.imshow('Debug', debugImg)
-    cv2.waitKey(0)
+        # Run inference
+        im_pil = Image.open(CAMERA_IMAGE).convert('RGB')
+        w, h = im_pil.size
+        orig_size = torch.tensor([w, h])[None].to(DEVICE)
+
+        transforms = T.Compose([
+            T.Resize((640, 640)),
+            T.ToTensor(),
+        ])
+        im_data = transforms(im_pil)[None].to(DEVICE)
+
+        output = model(im_data, orig_size)
+        labels, boxes, scores = output
+        print(f'[{iteration}] Ran inference')
+
+        # Coordinate transform
+        debugImg = cv2.imread(CAMERA_IMAGE)
+        loc.localize(debugImg)
+
+        # Draw inference boxes
+        scr = scores[0]
+        lab = labels[0][scr > INF_TH]
+        box = boxes[0][scr > INF_TH]
+        scrs = scores[0][scr > INF_TH]
+        for _, b in enumerate(box):
+            xy = b.detach().numpy().astype(np.int32)
+            cv2.rectangle(debugImg, xy[0:2], xy[2:4], (0, 0, 255), 2)
+
+        cv2.imwrite(DEBUG_IMAGE, debugImg)
+        # cv2.imshow('Debug', debugImg)
+        # cv2.waitKey(0)
+        print(f'[{iteration}] Write debug image')
+
+        iteration += 1
 
 if __name__ == '__main__':
     main()
